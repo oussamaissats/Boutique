5e4a145cecdd423868b8664fb27d2203682c898d
oussama <mohamedoussamabenjazia@gmail.com>
2022-05-16 23:01:42 +0200

    first commit

---
+++ scarpelle/src/data.js
@@ -0,0 +1,47 @@
+const data = {
+    products: [
+        {
+            name: 'STAU MARRON',
+            slug:"StauMarron",
+            Category:'Sneaker', 
+            image: '/images/p1.jpg', 
+            price: 80, 
+            countInStock: 10, 
+            brand:'Stau', 
+            rating:4.5,
+            numReviews:10,
+            description: 'Matière: 100% cuir extérieur et intérieur  Semelle Italienne qualité premium  Pointures 39 40 41 42 43 44 45 Pour commander appeler le 51 050 740 ou bien envoyer le modèle désiré en message privé votre adresse et votre numéro de téléphone Livraison gratuite sur le Grand Tunis ',
+        
+        },
+
+        {
+            name: 'DERBI MARRON ',
+            slug:"DerbiMarron",
+            Category:'Derbie', 
+            image: '/images/p9.jpg', 
+            price: 90, 
+            countInStock: 10, 
+            brand:'Stau', 
+            rating:4.5,
+            numReviews:10,
+            description: 'Matière: 100% cuir extérieur et intérieur  Semelle Italienne qualité premium  Pointures 39 40 41 42 43 44 45 Pour commander appeler le 51 050 740 ou bien envoyer le modèle désiré en message privé votre adresse et votre numéro de téléphone Livraison gratuite sur le Grand Tunis ',
+        
+        },
+
+        {
+            name: 'MOCASSIN BLEU',
+            slug:"MocassinBleu",
+            Category:'Mocassin', 
+            image: '/images/p12.jpg', 
+            price: 75, 
+            countInStock: 10, 
+            brand:'Stau', 
+            rating:4.5,
+            numReviews:10,
+            description: 'Matière: 100% cuir extérieur et intérieur  Semelle Italienne qualité premium  Pointures 39 40 41 42 43 44 45 Pour commander appeler le 51 050 740 ou bien envoyer le modèle désiré en message privé votre adresse et votre numéro de téléphone Livraison gratuite sur le Grand Tunis ',
+        
+        },
+    ],
+};
+
+export default data ; 
---
+++ scarpelle/src/screens/HomeScreen.js
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+//import data from "../data";
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import logger from 'use-reducer-logger' ; 
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return { ...state, loading: true };
+      case 'FETCH_SUCCESS':
+        return { ...state, products: action.payload, loading: false };
+      case 'FETCH_FAIL':
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
+
+function HomeScreen() {
+    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+        products: [],
+        loading: true,
+        error: '',
+      });
+  /*state*/
+  //const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+    dispatch({ type: 'FETCH_REQUEST' });
+    try {
+      const result = await axios.get('/api/products');
+      dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+    } catch (err) {
+      dispatch({ type: 'FETCH_FAIL', payload: err.message });
+    }
+
+      //setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Nos produits</h1>
+      <div className="products">
+        {loading? ( 
+        
+        <div>Loading...</div>
+        ): error? ( 
+        <div>{error}</div>
+ ):(
+        products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>{product.price} DT</strong>
+              </p>
+              <button>Ajouter au panier</button>
+            </div>
+          </div>
+        
+        ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
